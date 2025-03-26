f6a064321b8e2d72da00d8f95a879acfc7439745
Saad Hasan <136428421+saadhasan07@users.noreply.github.com>
2025-03-26 23:39:58 +0100

    making header

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hasan + Haider</h1>
+      <h1>Saad + Haider</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
